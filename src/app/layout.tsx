8324998c8b3113645cbbb50bd45a31230214a347
Noah luwita <65409682+Luwita@users.noreply.github.com>
2024-06-25 06:49:17 -0700

    headers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className=" ">
+          <div className="container flex justify-between py-4 px-6 nx-auto ">
+          <Link href={"/"} className="font-bold">Zed Jobs</Link>
+          <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-nd">
+           <Link className="bg-gray-200" href={'/login'}>Login</Link>
+           <Link className="bg-blue-600 text-white" href={'/new-listing'}>Post a Job</Link>
+        </nav>
+          </div>
+          
+        </header>
+        
+        {children}
+        </body>
     </html>
   );
 }
